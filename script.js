4b5334ff5b01fddbb1be181fa3ec2bfb2ce872c1
kalneyad <59976385+kalneyad@users.noreply.github.com>
2020-02-12 14:05:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function racer(name, speed, handling) {
+  var that = this;
+  this.name = name;
+  this.speed = speed;
+  this.handling = handling;
+  this.element = document.createElement("div");
+  this.element.classList.add("divstyle");
+  this.element.addEventListener("click", function(){that.changediv();})
+  this.element.innerHTML = "Name: "+this.name+"<br/>"+ "Speed: "+this.speed+"<br/>"+ "Handling: "+this.handling;
+  document.body.appendChild(this.element);
+}
+
+racer.prototype.changediv = function(){
+  if (this.handling==1){
+    this.element.style.outlineColor= "#FF0000";
+    this.speed = this.speed+5;
+  this.element.innerHTML = "Name: "+this.name+"<br/>"+ "Speed: "+this.speed+"<br/>"+ "Handling: "+this.handling;
+  }else{
+  this.speed = this.speed+5;
+  this.handling = this.handling-2;
+  this.element.innerHTML = "Name: "+this.name+"<br/>"+ "Speed: "+this.speed+"<br/>"+ "Handling: "+this.handling;}
+ }
+var racer1 = new racer("Alex", 60, 11);
+var racer2 = new racer("John", 40, 19);
+var racer3 = new racer("Sam", 50, 17);
